bbab0d5565e6c6f639f0dc2166647b631f16036a
이예서 <yeseo@iyeseos-MacBook-Pro.local>
2025-06-15 14:11:00 +0900

    fix:nvim startify bookmark path

--- nvim/lua/custom-vim-startify-config.lua
+++ nvim/lua/custom-vim-startify-config.lua
@@ -1,17 +1,15 @@
 return {
-  {
-    'mhinz/vim-startify',
-    config = function()
-      vim.g.startify_bookmarks = { '~/personal/minseon9.github.io/_wiki/' }
-      vim.g.startify_session_sort = 1
-      vim.g.startify_lists = {
-        { type= 'sessions',  header= {'   Sessions'}       },
-        { type= 'files',     header= {'   MRU'}            },
-        { type= 'bookmarks', header= {'   Bookmarks'}      },
-        { type= 'commands',  header= {'   Commands'}       },
-      }
-    end,
-  },
+	{
+		"mhinz/vim-startify",
+		config = function()
+			vim.g.startify_bookmarks = { "~/minseon9.github.io/_wiki/index.md" }
+			vim.g.startify_session_sort = 1
+			vim.g.startify_lists = {
+				{ type = "sessions", header = { "   Sessions" } },
+				{ type = "files", header = { "   MRU" } },
+				{ type = "bookmarks", header = { "   Bookmarks" } },
+				{ type = "commands", header = { "   Commands" } },
+			}
+		end,
+	},
 }
-
-
